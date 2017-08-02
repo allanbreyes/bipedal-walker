ce76ead354bc5db47ad64c6a53ce5949df17cf03
Allan Reyes <allanbreyes@gmail.com>
2017-08-02 08:57:35 -0700

    Fix typo

--- ddpg/agent_test.py
+++ ddpg/agent_test.py
@@ -4,7 +4,7 @@ import numpy as np
 from agent import Agent
 
 
-class MemoryTest(unittest.TestCase):
+class AgentTest(unittest.TestCase):
     def testValidateExperience(self):
         agent = Agent('test', None)
         self.assertTrue(agent.validate_experience(np.array([0, 1, 2, 3, 4])))
